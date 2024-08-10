fc3aafb3e7e37ef0e712dfb0e9543a9748bb6777
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-08-09 21:37:24 -0300

    add request to endopoint `/api/rooms/{id}`

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,15 +1,27 @@
-import { useNavigate } from "react-router-dom";
-import amaLogo from "../assets/ama-logo.svg";
 import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import amaLogo from "../assets/ama-logo.svg";
+import { createRoom } from "../http/create-room";
 
 export function CreateRoom() {
   const navigate = useNavigate();
 
-  function handleCreateRoom(data: FormData) {
+  async function handleCreateRoom(data: FormData) {
     const theme = data.get("theme")?.toString();
 
-    console.log(theme);
-    navigate("/room/dajfdajkfja");
+    if (!theme) {
+      return;
+    }
+
+    try {
+      const { roomId } = await createRoom({ theme });
+
+      navigate(`/room/${roomId}`);
+    } catch (err) {
+      console.error(err);
+      toast.error("Falha ao Criar Sala");
+    }
   }
 
   return (
@@ -31,6 +43,7 @@ export function CreateRoom() {
             placeholder="Nome da sala"
             autoComplete="off"
             className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500 "
+            required
           />
           <button
             type="submit"
