ac963f5c922ccc98fca7186694ae376a5ad6effb
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-08-12 11:13:13 -0300

    add function `getRoomMesssages`

--- web/src/http/get-room-messages.ts
+++ web/src/http/get-room-messages.ts
@@ -1,5 +1,5 @@
 interface GetRoomMessagesRequest {
-  roomId: string;
+  roomId: string
 }
 
 export interface GetRoomMessagesResponse {
@@ -8,32 +8,28 @@ export interface GetRoomMessagesResponse {
     text: string;
     amountOfReactions: number;
     answered: boolean;
-  }[];
+  }[]
 }
 
-export async function getRoomMessages({
-  roomId,
-}: GetRoomMessagesRequest): Promise<GetRoomMessagesResponse> {
-  const response = await fetch(
-    `${import.meta.env.VITE_APP_API_URL}/rooms/${roomId}/messages`
-  );
+export async function getRoomMessages({ roomId }: GetRoomMessagesRequest): Promise<GetRoomMessagesResponse> {
+  const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/rooms/${roomId}/messages`)
 
   const data: Array<{
-    id: string;
-    room_id: string;
-    message: string;
-    reaction_count: number;
-    answered: boolean;
-  }> = await response.json();
+    id: string
+    room_id: string
+    message: string
+    reaction_count: number
+    answered: boolean
+  }> = await response.json()
 
   return {
-    messages: data.map((item) => {
+    messages: data.map(item => {
       return {
         id: item.id,
         text: item.message,
         amountOfReactions: item.reaction_count,
         answered: item.answered,
-      };
-    }),
-  };
+      }
+    })
+  }
 }
